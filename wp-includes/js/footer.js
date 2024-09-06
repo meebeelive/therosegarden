f1598656b82bc32035fa6f8e66d6ac37516f4bd2
meebeelive <amittokarim@gmail.com>
2024-09-07 00:45:40 +0530

    updates

--- wp-includes/js/footer.js
+++ wp-includes/js/footer.js
@@ -9,7 +9,7 @@ let appFooter = `
 <section id="text-3" class="widget widget_text"><h2 class="widget-title">Contact Info</h2> <div class="textwidget"><p>Vazhayila, Trivandrum,<br/>
 Kerala, India.<br/>
 +91 938 777 5006<br/>
-<a href="../cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="91f2feffe5f0f2e5d1f6f0e3f5f4ffbff2fefc">rehnarajan@gmail.com</a>
+<a href="../cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="91f2feffe5f0f2e5d1f6f0e3f5f4ffbff2fefc">rajan.rehna@gmail.com</a>
 </div>
 </section> </aside>
 </div>
